13bb66c5dd298b9ff47628366bc88d57c842f49b
Urva Suthar <urvasuthar2@gmail.com>
2023-04-15 01:27:15 +0530

    css changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { TextInput } from 'react-native';
+import Timetable from './Timetable';
 
 
 const HomeScreen = ({ navigation }) => {
@@ -8,7 +9,7 @@ const HomeScreen = ({ navigation }) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: 'Test',
+      headerTitle: '',
       headerShown: false,
     });
   }, []);
@@ -19,16 +20,16 @@ const HomeScreen = ({ navigation }) => {
         className='flex-row pb-3 mt-5 items-center space-x-3 mx-4 border border-black rounded-lg font-medium border-r-2 border-b-2'
       >
         <Image
-        source={{uri:"https://thumbs.dreamstime.com/z/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg"}}
-        className='h-10 w-10 bg-red-400 m-2 -mr-1 rounded-full'
+          source={{ uri: "https://thumbs.dreamstime.com/z/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg" }}
+          className='h-10 w-10 bg-red-400 m-2 -mr-1 rounded-full'
         />
         <View className='flex-1'>
-        <Text className='text-bold text-gray-500 mt-1 text-lg'>
-          Hello 👋,
-        </Text>
-        <Text className='text-bold text-xl'>
-        Name Here
-        </Text>
+          <Text className='text-bold text-gray-500 mt-1 text-lg'>
+            Hello 👋,
+          </Text>
+          <Text className='text-bold text-xl'>
+            Name Here
+          </Text>
         </View>
       </View>
 
@@ -36,13 +37,23 @@ const HomeScreen = ({ navigation }) => {
         <Text className="text-xl text-gray-800 font-semibold m-3">Mark Attendance</Text>
         {/* create a button to enable Location */}
         <TouchableOpacity
-        className="items-left m-2"
-        activeOpacity={0.7}
+          className="items-left m-2 bg-yellow-300 p-3 text-sm border border-black rounded-lg font-medium border-r-2 border-b-2 justify-center pl-8 w-2/4"
+
+          activeOpacity={0.7}
         >
-        <Text className="bg-yellow-300 p-3 text-sm border border-black rounded-lg font-medium border-r-2 border-b-2 justify-center pl-8 w-2/4">Enable Location</Text>
+          <Text
+          >Enable Location</Text>
         </TouchableOpacity>
       </View>
-      
+      <TouchableOpacity
+        className="items-left mx-8 bg-red-300 p-3 text-sm border border-black rounded-lg font-medium border-r-2 border-b-2 justify-center w-1/4"
+        activeOpacity={0.7}
+        onPress={() => { navigation.navigate(Timetable) }}
+      >
+        <Text
+        >TimeTable</Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   )
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,15 @@
 import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 import React, { useState, useLayoutEffect } from 'react'
-import {auth} from '../firebase.js';
+import { auth } from '../firebase.js';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 
-const LoginScreen = ({navigation}) => {
-  const nav = ()=>{navigation.replace("Home")};
+const LoginScreen = ({ navigation }) => {
+  const nav = () => { navigation.replace("Home") };
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: 'Test',
@@ -17,27 +17,27 @@ const LoginScreen = ({navigation}) => {
     });
   }, []);
 
-  const RegisterUser = async ()=>{ 
+  const RegisterUser = async () => {
     await createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      console.log(userCredential)
-      alert(user+" created successfully!")
-      
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage +" "+ auth);
-      RegisterUser()
-      // ..
-    });
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(userCredential)
+        alert(user + " created successfully!")
+
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage + " " + auth);
+        RegisterUser()
+        // ..
+      });
   }
 
-  const LoginUser =  ()=>{
-     signInWithEmailAndPassword(auth, email, password)
+  const LoginUser = () => {
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
@@ -46,34 +46,36 @@ const LoginScreen = ({navigation}) => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.warn(errorCode +" "+ auth);
-      }); 
-  } 
-  
+        console.warn(errorCode + " " + auth);
+      });
+  }
+
 
 
   return (
     <View className="flex-1 items-center justify-center bg-stone-100">
       <View className="flex-col items-center p-2 px-4 bg-yellow-300 border border-black rounded-md border-r-4 border-b-4">
-        <Text className="text-xl font-bold m-2">Login</Text>  
+        <Text className="text-xl font-bold m-2">Login</Text>
         <TextInput
           placeholder='Email'
           className="w-60 p-2  bg-slate-100 my-2 border border-black border-r-2 border-b-2 placeholder:text-sm hover:border-spacing-2 rounded"
-          value = {email}
-          onChangeText = {(text) => setEmail(text)}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
         />
         <TextInput
           placeholder='Password'
           className="w-60 p-2 bg-slate-100 mb-3 border border-black border-r-2 border-b-2 placeholder:text-sm hover:border-spacing-2 rounded "
-          value = {password}
-          onChangeText = {(text) => setPassword(text)}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
           secureTextEntry={true}
         />
 
         <TouchableOpacity
-        onPress = {()=>{navigation.replace('Home')}}
+          onPress={() => { navigation.replace('Home') }}
+          className="bg-blue-300 p-2 px-4 mb-2 text-sm border border-black rounded-lg font-regular border-r-2 border-b-2 hover:bg-blue-500 hover:text-white"
         >
-          <Text className="bg-blue-300 p-2 px-4 mb-2 text-sm border border-black rounded-lg font-regular border-r-2 border-b-2 hover:bg-blue-500 hover:text-white">Submit</Text>
+          <Text
+          >Submit</Text>
         </TouchableOpacity>
 
         {/* <TouchableOpacity
