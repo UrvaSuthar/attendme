0515571a1b6e0f2c006dbf0802ac63c91c6e1f3c
Urva Suthar <urvasuthar2@gmail.com>
2023-04-15 01:28:05 +0530

    timetable screen added to stack

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import Timetable from "./screens/Timetable";
 import React from 'react';
 
 const Stack = createStackNavigator();
@@ -13,6 +14,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={ LoginScreen } />
         <Stack.Screen name="Home" component={ HomeScreen } />
+        <Stack.Screen name="Timetable" component={ Timetable } />
       </Stack.Navigator>
     </NavigationContainer>
   );
