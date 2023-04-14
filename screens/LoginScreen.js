3b5e3474a4a91cc784483557c013da47b1071c68
Urva Suthar <urvasuthar2@gmail.com>
2023-04-14 21:37:22 +0530

    header removed

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,8 @@
 import { View, Text, TouchableOpacity, TextInput } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import {auth} from '../firebase.js';
 import { createUserWithEmailAndPassword } from "firebase/auth";
-
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 
 const LoginScreen = ({navigation}) => {
@@ -10,7 +10,15 @@ const LoginScreen = ({navigation}) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   
-  const RegisterUser = async ()=>{ await createUserWithEmailAndPassword(auth, email, password)
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Test',
+      headerShown: false,
+    });
+  }, []);
+
+  const RegisterUser = async ()=>{ 
+    await createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
@@ -23,32 +31,49 @@ const LoginScreen = ({navigation}) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       alert(errorMessage +" "+ auth);
+      RegisterUser()
       // ..
     });
   }
 
+  const LoginUser =  ()=>{
+     signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.warn(errorCode +" "+ auth);
+      }); 
+  } 
+  
+
+
   return (
     <View className="flex items-center justify-center h-screen bg-stone-100">
       <View className="flex-col items-center p-3 bg-yellow-300 border border-black rounded-md border-r-4 border-b-4">
+        <Text className="text-2xl font-bold m-3">Login</Text>  
         <TextInput
           placeholder='Email'
-          className="w-80 p-3 rounded-xl bg-slate-100 my-2 p-1 border border-black border-r-2 border-b-2 placeholder:text-sm hover:border-spacing-2 rounded"
+          className="w-60 p-2  bg-slate-100 my-2 border border-black border-r-2 border-b-2 placeholder:text-sm hover:border-spacing-2 rounded"
           value = {email}
           onChangeText = {(text) => setEmail(text)}
         />
         <TextInput
           placeholder='Password'
-          className="w-80 p-3 rounded-xl bg-slate-100 my-2 p-1 border border-black border-r-2 border-b-2 placeholder:text-sm hover:border-spacing-2 rounded "
+          className="w-60 p-2 bg-slate-100 mb-3 border border-black border-r-2 border-b-2 placeholder:text-sm hover:border-spacing-2 rounded "
           value = {password}
           onChangeText = {(text) => setPassword(text)}
           secureTextEntry={true}
         />
 
         <TouchableOpacity
-
-
+        onPress = {LoginUser}
         >
-          <Text className="bg-blue-300 p-2 text-sm border border-black rounded-lg font-light border-r-2 border-b-2 hover:bg-blue-500 hover:text-white">Login</Text>
+          <Text className="bg-blue-300 p-2 px-4 text-sm border border-black rounded-lg font-light border-r-2 border-b-2 hover:bg-blue-500 hover:text-white">Login</Text>
         </TouchableOpacity>
 
         {/* <TouchableOpacity
